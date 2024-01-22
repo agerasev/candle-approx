673b174cbe99dffa3105a9caf7386e3d23c93c44
Alexey Gerasev <alexey.gerasev@gmail.com>
2024-01-22 12:58:28 +0700

    Use traits for approx crate

--- src/tests.rs
+++ src/tests.rs
@@ -1,37 +1,38 @@
-use crate::{assert_almost_eq, EPS, REL_EPS};
-use candle::{Device::Cpu, Tensor};
+use crate::{assert_abs_diff_eq, assert_relative_eq};
+use candle_core::{Device::Cpu, Tensor};
 
-const SHAPE: (usize, usize, usize) = (100, 200, 300);
+const EPS: f64 = 0.98 * crate::DEFAULT_EPSILON;
+const REL_EPS: f64 = 0.98 * crate::DEFAULT_MAX_RELATIVE;
+const SHAPE: (usize, usize, usize) = (20, 30, 40);
 
 #[test]
 fn abs_diff() {
-    let a = Tensor::rand(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
-    let b = a
-        .add(&a.rand_like(-0.99 * EPS, 0.99 * EPS).unwrap())
-        .unwrap();
-    assert_almost_eq!(a, b);
+    let a = Tensor::randn(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
+    let noise = a.rand_like(-EPS, EPS).unwrap();
+    let b = a.add(&noise).unwrap();
+    assert_abs_diff_eq!(a, b);
 }
 
 #[test]
 #[should_panic]
 fn abs_diff_panic() {
-    let a = Tensor::rand(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
+    let a = Tensor::randn(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
     let b = (&a + 2.0 * EPS).unwrap();
-    assert_almost_eq!(a, b);
+    assert_abs_diff_eq!(a, b);
 }
 
 #[test]
 fn relative() {
-    let a = Tensor::rand(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
-    let c = 1.0 + 0.99 * REL_EPS;
-    let b = a.mul(&a.rand_like(1.0 / c, c).unwrap()).unwrap();
-    assert_almost_eq!(a, b);
+    let a = Tensor::randn(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
+    let factor = 1.0 + REL_EPS;
+    let b = a.mul(&a.rand_like(1.0 / factor, factor).unwrap()).unwrap();
+    assert_relative_eq!(a, b);
 }
 
 #[test]
 #[should_panic]
 fn relative_panic() {
-    let a = Tensor::rand(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
+    let a = Tensor::randn(0.0f32, 1.0, SHAPE, &Cpu).unwrap();
     let b = (&a * (1.0 + 2.0 * REL_EPS)).unwrap();
-    assert_almost_eq!(a, b);
+    assert_relative_eq!(a, b);
 }
